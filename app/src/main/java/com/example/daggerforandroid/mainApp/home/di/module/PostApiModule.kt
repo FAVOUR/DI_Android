559e490ee88c9fcc090ec2f112a3f52a964f8537
FAVOUR <truelyfavoured@gmail.com>
2020-05-17 13:34:21 +0100

    Done with the implementation of the recycler view for displaying the post

--- app/src/main/java/com/example/daggerforandroid/mainApp/home/di/module/PostApiModule.kt
+++ app/src/main/java/com/example/daggerforandroid/mainApp/home/di/module/PostApiModule.kt
@@ -1,5 +1,7 @@
 package com.example.daggerforandroid.mainApp.home.di.module
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.daggerforandroid.mainApp.home.recyclerview.PostsRecyclerAdapter
 import com.example.daggerforandroid.mainApp.home.service.PostApi
 import dagger.Module
 import dagger.Provides
@@ -11,5 +13,10 @@ class PostApiModule {
     @Provides
     fun providesPosts(retrofit:Retrofit): PostApi {
         return retrofit.create(PostApi::class.java)
+    }
+
+    @Provides
+    fun providesRecyclerView():PostsRecyclerAdapter{
+        return PostsRecyclerAdapter()
     }
 }
--- app/src/main/java/com/example/daggerforandroid/mainApp/home/fragment/PostFragment.kt
+++ app/src/main/java/com/example/daggerforandroid/mainApp/home/fragment/PostFragment.kt
@@ -9,14 +9,19 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.daggerforandroid.R
 import com.example.daggerforandroid.application.SessionManager
 import com.example.daggerforandroid.application.di.viewmodels.ViewModelProviderFactory
 import com.example.daggerforandroid.mainApp.home.model.Post
 import com.example.daggerforandroid.mainApp.home.model.Resource
+import com.example.daggerforandroid.mainApp.home.recyclerview.PostsRecyclerAdapter
 import com.example.daggerforandroid.mainApp.home.viewmodel.PostViewModel
 import com.google.gson.Gson
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_posts.*
 import javax.inject.Inject
 
 class PostFragment : DaggerFragment() {
@@ -24,19 +29,31 @@ class PostFragment : DaggerFragment() {
     @Inject
     lateinit var viewmodelProviderFactory: ViewModelProviderFactory
     lateinit var  postViewModel: PostViewModel
+
+    @Inject
+    lateinit var postsRecyclerAdapter: PostsRecyclerAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile,container,false)
+        return inflater.inflate(R.layout.fragment_posts,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         postViewModel= ViewModelProviders.of(this,viewmodelProviderFactory)[PostViewModel::class.java]
-
+        setuprecyclerView()
         subScribeToObserver()
+    }
+
+
+    fun setuprecyclerView (){
+        recycler_view.adapter =postsRecyclerAdapter
+        var layoutManager =LinearLayoutManager(activity)
+        recycler_view.layoutManager=layoutManager
+        var itemDecorator = DividerItemDecoration(recycler_view.context,layoutManager.orientation)
+        recycler_view.addItemDecoration(itemDecorator)
     }
 
      fun subScribeToObserver(){
@@ -45,7 +62,26 @@ class PostFragment : DaggerFragment() {
          postViewModel.getPost().observe(this, object :Observer<Resource<List<Post?>>>{
              override fun onChanged(t: Resource<List<Post?>>) {
 
-                 Log.e("the Post date gotten ", Gson().toJson(t))
+                 Log.e("the Post data gotten ", Gson().toJson(t))
+                 if(t != null) {
+                     when (t.status) {
+
+                         Resource.Status.LOADING -> {
+                             Log.e("the Post data Loading  ",".........")
+
+                         }
+
+                         Resource.Status.SUCCESS -> {
+                             postsRecyclerAdapter.setPosts(t.data)
+                         }
+
+                         Resource.Status.ERROR -> {
+                             Log.e("the Post data Error ", Gson().toJson(t))
+
+                         }
+                     }
+
+                 }
              }
          })
 
