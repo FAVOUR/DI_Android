a69dd24d21c9bab904585e1092f8227c762d5516
FAVOUR <truelyfavoured@gmail.com>
2020-05-19 06:42:39 +0100

    finally fixed the back stack issue for post fragment

--- app/src/main/java/com/example/daggerforandroid/mainApp/auth/di/module/AuthApiModule.kt
+++ app/src/main/java/com/example/daggerforandroid/mainApp/auth/di/module/AuthApiModule.kt
@@ -1,12 +1,23 @@
 package com.example.daggerforandroid.mainApp.auth.di.module
 
+import com.example.daggerforandroid.mainApp.auth.model.User
 import com.example.daggerforandroid.mainApp.auth.services.AuthApi
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
+import javax.inject.Named
 
 @Module
 class AuthApiModule {
+
+    @AuthScope
+    @Provides
+    @Named("auth_user") //This is how to differentiate a method that has the same type so there can possibly identify teh variable been referred to
+    fun someUser(): User? {
+        return User()
+    }
+
+
     @AuthScope
     @Provides
     fun getApi (retrofit: Retrofit):AuthApi{
